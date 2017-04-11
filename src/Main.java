f0bf1e56ab2c41a33d8258a108a4c71988f6ab9a
Johannes <altnas@student.chalmers.se>
2017-04-11 12:19:19 +0200

    hheheh

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main (String[] args) {
 
-        System.out.println("How do conflicts wordings hejeeeeeee?");
-
-        System.out.println("How do conflicts workings!");
-        System.out.print("Super bad");
+        
 
     }
 }
