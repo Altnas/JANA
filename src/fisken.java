48d289ee22f957e53406656a3e8955c8e74d02b6
Altnas <altnas@student.chalmers.se>
2017-04-11 10:41:22 +0200

    Update fisken.java

--- src/fisken.java
+++ src/fisken.java
@@ -2,10 +2,10 @@
  * Created by gussj on 2017-04-11.
  */
 public class fisken {
-<<<<<<< HEAD
+
  public static void main(String[] args){
         System.out.println("Fish sauce");
         System.out.println("Detta är min text");
->>>>>>> 40eab88d92d45f9bfcc7b1431e826d49e4069b4c
+
     }
 }
