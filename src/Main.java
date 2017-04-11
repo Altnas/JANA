3f2383e47d69eea4d212c21058ad80bb0332869f
SpaceyCat <ericlin@student.chalmers.se>
2017-04-11 11:16:21 +0200

    hejej

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("How do conflicts workings?");
+        System.out.println("How do conflicts workingsn hejeeeeeee?");
     }
 }
