9be7777fbe0a4c4eea507de1a71103975d811aad
gussj <gussjos@student.chalmers.se>
2017-04-11 10:58:31 +0200

    22

--- src/fisken.java
+++ src/fisken.java
@@ -6,8 +6,5 @@ public class fisken {
  public static void main(String[] args){
         System.out.println("Fish sauce");
         System.out.println("Detta är min text");
- 
-        System.out.println("Fish sauce");
-        System.out.println("Detta är min text");
     }
 }
