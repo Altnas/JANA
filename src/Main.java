adf50244a93a53a49ba342df47a07ff8307c1f73
Johannes <altnas@student.chalmers.se>
2017-04-11 09:54:24 +0200

    does this actually work?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Fuck this shit yo");
+        System.out.println("How does conflicts work?");
+
     }
 }
