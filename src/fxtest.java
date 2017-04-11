65800f590d327cbabe9ac41e4700722d9c3b7c28
SpaceyCat <ericlin@student.chalmers.se>
2017-04-11 11:51:55 +0200

    sdfsdfsdfsd

--- src/fxtest.java
+++ src/fxtest.java
@@ -21,11 +21,8 @@ public class fxtest extends Application{
         grid.add(text,0,0,2,1);
         //Scene scene1 = new Scene(, 600,600);
         primaryStage.setScene(scene);
-<<<<<<< HEAD
-        //Gustaf är kortast
-=======
-        //Gustaf är kortare //kusk
->>>>>>> origin/master
+
+        //Suk min kug
 
 
         primaryStage.show();
