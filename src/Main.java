4d6443a2aef85a0cdd6275d8fdd1baeaf1124fcf
gussj <gussjos@student.chalmers.se>
2017-04-11 10:01:44 +0200

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("How does conflicts work?");
-
+        System.out.println("How do conflicts work?");
     }
 }
